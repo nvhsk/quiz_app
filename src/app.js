8f87b0ab1d96999c905d82f265f5fe07e78aba95
Iryna Novokhatska <iryna.nov@icloud.com>
2023-07-26 10:48:54 +0200

    done

--- src/app.js
+++ src/app.js
@@ -18,20 +18,23 @@
 // });
 // // END TASK 1
 
-// document.addEventListener("DOMContentLoaded", () => {
-//   const showAnswer = document.querySelector('[data-js="show-answer-button"]');
-//   const hideAnswer = document.querySelector('[data-js="hide-answer-button"]');
-//   const answer = document.querySelector('[data-js="answer"]');
-
-//   showAnswer.addEventListener("click", () => {
-//     answer.classList.toggle("invisible");
-//     showAnswer.classList.toggle("invisible");
-//     hideAnswer.classList.toggle("invisible");
-//   });
-
-//   hideAnswer.addEventListener("click", () => {
-//     answer.classList.toggle("invisible");
-//     showAnswer.classList.toggle("invisible");
-//     hideAnswer.classList.toggle("invisible");
-//   });
-// });
+// TASK 1
+// Gettings Elements from HTML
+document.addEventListener("DOMContentLoaded", () => {
+  const showAnswer = document.querySelector('[data-js="show-answer-button"]');
+  const hideAnswer = document.querySelector('[data-js="hide-answer-button"]');
+  const answer = document.querySelector('[data-js="answer"]');
+  // Show answer
+  showAnswer.addEventListener("click", () => {
+    answer.classList.toggle("invisible");
+    showAnswer.classList.toggle("invisible");
+    hideAnswer.classList.toggle("invisible");
+  });
+  // Hide answer
+  hideAnswer.addEventListener("click", () => {
+    answer.classList.toggle("invisible");
+    showAnswer.classList.toggle("invisible");
+    hideAnswer.classList.toggle("invisible");
+  });
+});
+// END TASK 1
